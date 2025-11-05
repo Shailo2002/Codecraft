75eded733af6c3cfbaec0dc7bc0ea902be574c9d
shailesh <shailesh364465@gmail.com>
2025-11-05 20:40:12 +0530

    first commit

---
+++ app/_components/Header.tsx
@@ -0,0 +1,30 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import React from "react";
+
+const menuOptions = [
+    {name: "Pricing", path:"/pricing"},{
+        name:"Contact Us", path:"/contact-us"
+    }
+]
+
+function Header() {
+  return (
+    <div className="flex justify-between items-center p-4 shadow-lg">
+      {/* logo  */}
+      <Image src={"/logo.svg"} alt="logo" width={140} height={140} />
+
+      {/* menu options */}
+      <div className="flex gap-2">
+        {menuOptions.map((menu,key) => (
+            <Button variant={"ghost"} key={key}>{menu?.name}</Button>
+        ))}
+      </div>
+      
+      {/* get started button */}
+      <Button>Get Started</Button>
+    </div>
+  );
+}
+
+export default Header;
---
+++ app/_components/Hero.tsx
@@ -0,0 +1,84 @@
+"use client"
+
+import { Button } from "@/components/ui/button";
+import {
+  ArrowUp,
+  HomeIcon,
+  ImagePlus,
+  Key,
+  LayoutDashboard,
+  User,
+} from "lucide-react";
+import { useState } from "react";
+
+const suggestion = [
+  {
+    label: "Dashboard",
+    prompt:
+      "Create an analytics dashboard to track customers and revenue data for a SaaS",
+    icon: LayoutDashboard,
+  },
+  {
+    label: "SignUp Form",
+    prompt:
+      "Create a modern sign up form with email/password fields, Google and Github login options, and terms checkbox",
+    icon: Key,
+  },
+  {
+    label: "Hero",
+    prompt:
+      "Create a modern header and centered hero section for a productivity SaaS. Include a badge for feature announcement, a title with a subtle gradient effect, subtitle, CTA, small social proof and an image.",
+    icon: HomeIcon,
+  },
+  {
+    label: "User Profile Card",
+    prompt:
+      "Create a modern user profile card component for a social media website",
+    icon: User,
+  },
+];
+
+function Hero() {
+    const [userInput, setUserInput] = useState<string>();
+  return (
+    <div className="flex flex-col justify-center items-center h-[80vh]">
+      {/* description */}
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="text-center text-6xl font-extrabold tracking-tight text-balance">
+          What should we design?
+        </h1>
+        <h4 className="text-gray-500 mt-2 text-xl tracking-tight">
+          Generate, Edit and Explore design with AI. Export to code.
+        </h4>
+      </div>
+
+      {/* input */}
+      <div className="w-full max-w-2xl p-5 mt-5 border rounded-2xl">
+        <textarea
+          placeholder="Describe your page design"
+          className="w-full  h-24 focus:outline-none focus:ring-0 resize-none"
+          onChange={(e) => setUserInput(e.target.value)}
+          value={userInput}
+        />
+        <div className="flex justify-between items-center">
+          <Button variant={"ghost"}>
+            <ImagePlus />
+          </Button>
+          <Button size={"icon"}>
+            <ArrowUp />
+          </Button>
+        </div>
+      </div>
+
+
+      {/* options */}
+      <div className="flex gap-3 mt-4">
+        {suggestion.map((value, index) => (
+            <Button variant={"outline"} onClick={() => setUserInput(value.prompt)}><value.icon/>{value.label}</Button>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
