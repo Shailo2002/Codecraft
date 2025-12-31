87a52dc9420e7b3a5fb15aa05bc8786cd801372f
shailesh parvadiya <shailesh364465@gmail.com>
2025-12-31 23:28:36 +0530

    payment branch added

--- app/api/cron/expire-premium/route.ts
+++ app/api/cron/expire-premium/route.ts
@@ -2,6 +2,10 @@ import { expirePremiumUsers } from "@/app/jobs/expirePremiumUsers";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  await expirePremiumUsers();
-  return NextResponse.json({ ok: true });
+  try {
+    await expirePremiumUsers();
+    return NextResponse.json({ ok: true });
+  } catch (error) {
+    return NextResponse.json({ ok: false });
+  }
 }
