db83ff7a816f8d4e957c6d47bffbd7d66f57a785
shailesh parvadiya <shailesh364465@gmail.com>
2026-01-06 22:35:56 +0530

    output path for prisma updated

--- app/actions/createProject.ts
+++ app/actions/createProject.ts
@@ -2,6 +2,7 @@
 import prisma from "@/lib/db";
 import { currentUser } from "@clerk/nextjs/server";
 import { v4 as uuidv4 } from "uuid";
+import { Prisma } from "@prisma/client";
 
 export default async function createProject({
   chatMessage,
@@ -14,49 +15,51 @@ export default async function createProject({
   }
 
   try {
-    const result = await prisma.$transaction(async (tx) => {
-      const dbUser = await tx.user.findUnique({
-        where: { email: userDetail.primaryEmailAddress?.emailAddress },
-      });
+    const result = await prisma.$transaction(
+      async (tx: Prisma.TransactionClient) => {
+        const dbUser = await tx.user.findUnique({
+          where: { email: userDetail.primaryEmailAddress?.emailAddress },
+        });
 
-      if (!dbUser) {
-        return { ok: false, error: "Unauthorized" };
+        if (!dbUser) {
+          return { ok: false, error: "Unauthorized" };
+        }
+
+        if (dbUser.credits <= 0) {
+          return { ok: false, error: "No credits remaining" };
+        }
+
+        const projectId = uuidv4();
+        const frameId = crypto.randomUUID().slice(0, 8);
+
+        const newProject = await tx.project.create({
+          data: { projectId, userId: dbUser.id },
+        });
+
+        const newFrame = await tx.frame.create({
+          data: {
+            frameId,
+            projectId: newProject.id,
+            designCode: "<div> test code </div>",
+          },
+        });
+
+        await tx.chatMessage.create({
+          data: {
+            chatMessage,
+            userId: dbUser.id,
+            frameId: newFrame.id,
+          },
+        });
+
+        await tx.user.update({
+          where: { id: dbUser.id },
+          data: { credits: { decrement: 1 } },
+        });
+
+        return { ok: true, projectId, frameId };
       }
-
-      if (dbUser.credits <= 0) {
-        return { ok: false, error: "No credits remaining" };
-      }
-
-      const projectId = uuidv4();
-      const frameId = crypto.randomUUID().slice(0, 8);
-
-      const newProject = await tx.project.create({
-        data: { projectId, userId: dbUser.id },
-      });
-
-      const newFrame = await tx.frame.create({
-        data: {
-          frameId,
-          projectId: newProject.id,
-          designCode: "<div> test code </div>",
-        },
-      });
-
-      await tx.chatMessage.create({
-        data: {
-          chatMessage,
-          userId: dbUser.id,
-          frameId: newFrame.id,
-        },
-      });
-
-      await tx.user.update({
-        where: { id: dbUser.id },
-        data: { credits: { decrement: 1 } },
-      });
-
-      return { ok: true, projectId, frameId };
-    });
+    );
 
     return result;
   } catch {
