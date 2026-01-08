eb0c2afad0f8343530ea0cfa9b1e324167b0b895
shailesh parvadiya <shailesh364465@gmail.com>
2026-01-08 17:32:18 +0530

    vercel.json updated for cron testing

--- app/api/cron/expire-premium/route.ts
+++ app/api/cron/expire-premium/route.ts
@@ -1,11 +1,13 @@
 import { expirePremiumUsers } from "@/app/jobs/expirePremiumUsers";
-import { NextResponse } from "next/server";
 
 export async function GET() {
+  console.log("CRON FIRED AT:", new Date().toISOString());
+
   try {
     await expirePremiumUsers();
-    return NextResponse.json({ ok: true });
-  } catch (error) {
-    return NextResponse.json({ ok: false });
+    return Response.json({ ok: true });
+  } catch (e) {
+    console.error("CRON ERROR", e);
+    return Response.json({ ok: false });
   }
 }
